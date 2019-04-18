1e16c460910718052974fb5f1768e00874b62487
Meghal Agrawal <educate1996@gmail.com>
2019-04-18 19:37:45 +0530

    added query  in react book component

--- client/src/component/BookList.js
+++ client/src/component/BookList.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import {gql} from 'apollo-boost';
 
+const getBooksQuery=gql`{
+  books{
+    name
+    id
+  }
+}
+`
+
 class BookList extends Component {
   render() {
     return (
